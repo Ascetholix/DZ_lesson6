cb4266feb885a5f3f8ccf65544cacf479351e2dd
Sangak <96950842+Ascetholix@users.noreply.github.com>
2022-09-17 23:48:28 +0300

    done task

--- task41/Program.cs
+++ task41/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.Clear();
+
+void FillArray(int[] arg)
+{
+    for (int i = 0; i < arg.Length; i++)
+    {
+        Console.Write($"Введите {arg.Length - i} число: ");
+        arg[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+int Positive(int[] arg)
+{
+    int pos = 0;
+    for (int i = 0; i < arg.Length; i++)
+    {
+        if (arg[i] > 0) pos++;
+    }
+    return pos;
+}
+Console.Write($"Введите  количество цифр: ");
+int len = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[len];
+
+FillArray(array);
+
+Console.WriteLine(Positive(array));
+
--- task43/Program.cs
+++ task43/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+Console.WriteLine("Программа по формеле y = k1 * x + b1 и y = k2 * x + b2 ");
+Console.WriteLine("Для нахождения пересечения");
+
+int[] k = new int[2];
+int[] b = new int[2];
+
+for (int i = 0; i < 2; i++)
+{
+    Console.Write($"Введите отрезки K{1 + i}: ");
+    k[i] = Convert.ToInt32(Console.ReadLine());
+}
+for (int i = 0; i < 2; i++)
+{
+    Console.Write($"Введите отрезки B{1 + i}: ");
+    b[i] = Convert.ToInt32(Console.ReadLine());
+}
+double B = b[0] - b[1];
+double K = k[1] - k[0];
+
+Console.WriteLine($"{K}");
+Console.WriteLine($"{B}");
+Console.WriteLine($"{B/K}");
